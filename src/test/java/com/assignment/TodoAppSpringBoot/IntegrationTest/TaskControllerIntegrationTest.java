1b845f0c20218fc1482ab93376544cabe90cf408
SandaluKarunasena <sandalu.18@cse.mrt.ac.lk>
2023-08-23 10:59:59 +0530

    Write integration tests

--- src/test/java/com/assignment/TodoAppSpringBoot/IntegrationTest/TaskControllerIntegrationTest.java
+++ src/test/java/com/assignment/TodoAppSpringBoot/IntegrationTest/TaskControllerIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.assignment.TodoAppSpringBoot.controllers;
+package com.assignment.TodoAppSpringBoot.IntegrationTest;
 
 import com.assignment.TodoAppSpringBoot.models.Task;
 import io.restassured.RestAssured;
@@ -42,5 +42,81 @@ public class TaskControllerIntegrationTest {
                 .body("completed", everyItem(instanceOf(boolean.class)));
     }
 
+    @Test
+    public void testGetTaskById() {
+        int targetId = 3;
 
+        // Perform the HTTP request
+        given()
+                .pathParam("id", targetId)
+                .when()
+                .get("/api/v1/tasks/{id}")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(targetId))
+                .body("task", equalTo("Write Integration tests With REST Assured"));
+    }
+
+    @Test
+    public void testCreateTask() {
+        Task newTask = new Task(3L, "New Integration Testing Task", false);
+        given()
+                .contentType(ContentType.JSON)
+                .body(newTask)
+                .when()
+                .post("/api/v1/tasks/")
+                .then()
+                .statusCode(200)
+                .body("task", equalTo(newTask.getTask()))
+                .body("id", notNullValue())
+                .body("task", notNullValue())
+                .body("completed", notNullValue())
+                .body("completed", equalTo(newTask.isCompleted())); // Add more assertions based on your data
+    }
+
+    @Test
+    public void testUpdateTask() {
+        Long taskId = 1L; // Replace with a valid task ID
+        Task updatedTask = new Task(taskId, "Updated Integration Testing Task", true);
+        given()
+                .contentType(ContentType.JSON)
+                .body(updatedTask)
+                .when()
+                .put("/api/v1/tasks/" + taskId)
+                .then()
+                .statusCode(200)
+                .body("task", equalTo(updatedTask.getTask()))
+                .body("completed", is(updatedTask.isCompleted())); // Add more assertions based on your data
+    }
+
+    @Test
+    public void testDeleteTask() {
+        Long existingTaskId = 45L;
+
+        // Perform the HTTP DELETE request
+        given()
+                .pathParam("id", existingTaskId)
+                .when()
+                .delete("/api/v1/tasks/{id}")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Delete Successful"));
+
+        // Verify that the task with the specified ID is no longer accessible
+        given()
+                .pathParam("id", existingTaskId)
+                .when()
+                .get("/api/v1/tasks/{id}")
+                .then()
+                .statusCode(200)
+                .body(not(hasValue(nullValue())));// Assuming you return 200 when the task is not found
+
+        // Verify that the list of all tasks no longer contains the deleted task
+        given()
+                .when()
+                .get("/api/v1/tasks")
+                .then()
+                .statusCode(404)
+                .body("id", not(hasItem(existingTaskId.intValue())));
+    }
 }
